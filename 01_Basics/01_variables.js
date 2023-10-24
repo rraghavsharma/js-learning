5386f8a99509094ea51280187daa760c9b863da2
Raghav Sharma <57595848+iraghavsharma18@users.noreply.github.com>
2023-10-24 18:14:10 +0000

    container config done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -3,9 +3,9 @@ let accountEmail = "raghav@gmail.com"
 var accountPassword = "1254"
 accountCity = "Gwalior"
 let accountState;
-// in js if can not assign variableName as what (const, let and var etc.), it automatically assign as var ----> not a good way
+// In js if can not assign variableName as what (const, let and var etc.), it automatically assign as var ----> not a good way
 // var use karne k do tarike h js m ----> use as (var or let variable)
-// in js we can or can't use semi-colon depends on us.
+// In js we can or can't use semi-colon depends on us.
 
 // accountId = 2 ----> Not allowed
 
