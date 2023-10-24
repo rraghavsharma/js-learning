32e1a81448e29f7143a09174aa4e74227dc4ee47
Raghav Sharma <57595848+iraghavsharma18@users.noreply.github.com>
2023-10-24 18:44:11 +0000

    container config done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+/*
+alert("hello") ----> we are using nodejs, not browser
+alert(3+3)
+if we use alert in browser console then this will show pop on screen but not show in nodejs
+javascript ka engine browser ke andar chupa hua h
+*/
+
+let name = "raghav"
+let age = 22
+let isloggedin = true
+
+
+/*
+<---- Datatypes ---->
+1. number --> 2 to power 53(approx)
+2. bigint --> for big number
+3. string --> ""(prefered), ''
+4. boolean --> true/false // use ya toh  uski credit card detail hai ya nhi hai
+5. null --> standalone value // alag se ek value h, null ek representation h ek empty value ka
+use matlab agar hum temperature check kar or temperature kuch aaye nhi toh hum null prefered karte hai
+6. undefined
+7. symbol --> unique
+8. object
+*/
