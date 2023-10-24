2ad4d7c026afc4ce637233228fa170763528b171
Raghav Sharma <57595848+iraghavsharma18@users.noreply.github.com>
2023-10-24 18:12:52 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 154
+let accountEmail = "raghav@gmail.com"
+var accountPassword = "1254"
+accountCity = "Gwalior"
+let accountState;
+// in js if can not assign variableName as what (const, let and var etc.), it automatically assign as var ----> not a good way
+// var use karne k do tarike h js m ----> use as (var or let variable)
+// in js we can or can't use semi-colon depends on us.
+
+// accountId = 2 ----> Not allowed
+
+accountEmail = "sharma@gamil.com"
+accountPassword = "5124"
+accountCity = "Bhopal"
+
+/*
+prefer not to use var 
+instead of var we use let
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
