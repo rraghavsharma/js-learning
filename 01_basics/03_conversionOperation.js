2d6348a2df925d96f8273b47cde9b2435a6a7e41
Raghav Sharma <57595848+iraghavsharma18@users.noreply.github.com>
2023-10-26 15:41:29 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+// let score = 23
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let sco = "33"
+// console.log(typeof sco);
+// console.log(typeof(sco));
+
+// let valueInNumber_01 = Number(sco) // always use capital N for number there and also javascript is case sensitive language
+// console.log(typeof valueInNumber_01);
+// console.log(valueInNumber_01); // 33
+
+// let scor = "33ab"
+// let valueInNumber_02 = Number(scor)
+// console.log(typeof valueInNumber_02);
+// console.log(valueInNumber_02);  // NaN
+
+// typeof null --> object
+// typeof undefined --> undefined
+
+// convert things(variables) into number (values are)
+// "33" --> 33
+// "33ab" --> NaN
+// "raghav" --> NaN
+// "" --> NaN
+// true --> 1, false --> 0
+// null --> 0
+// undefined --> undefined
+
+let isLoggedIn = 7
+let booleanIsLoggedIn = Boolean(isLoggedIn)  // always use capital B for Boolean there and also javascript is case sensitive language
+console.log(booleanIsLoggedIn);
+
+// convert things(variables) into Boolean
+// 1 --> true, 0 --> false, 7 --> true
+// "" --> false
+// "raghav" --> true
+// "false" --> true
+// "0" --> true
+// " " --> true
+// NaN --> false
+// null --> false
+// undefined --> false
+
+let number = 23
+let stringNumber = String(number)
+console.log(typeof stringNumber)
+console.log(stringNumber);
