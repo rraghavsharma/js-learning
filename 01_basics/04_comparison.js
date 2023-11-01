d00b8afa027dfcea2c935c328b476049a9f39b09
Raghav Sharma <57595848+iraghavsharma18@users.noreply.github.com>
2023-11-01 12:09:57 +0000

    conversion

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,3 +1,31 @@
-console.log(2 > 1);
-console.log(2 == 1);
-console.log(2 < 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+
+// problem comes when we didn't compare data types... sometimes it didn't give expected result
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("" > 1);
+
+// typescript gives us some rules esa karo esa mat karo
+
+// console.log(null < 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0); 
+// null is converted into in 0 & NaN
+
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+// ===
+// console.log("2" === 2);
+
+
+// Note : Avoid comparison between different types of data types
